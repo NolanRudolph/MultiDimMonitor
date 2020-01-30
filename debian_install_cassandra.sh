77c78e1bd32615ee5da677ea855e813b60eab444
NolanR <ngr@uoregon.edu>
2020-01-29 19:39:17 -0800

    Cassandra boots into battlegit add --all

---
+++ debian_install_cassandra.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Installs Apache Cassandra to /etc/cassandra
+# Log files: /var/log/cassandra/ and /var/lib/cassandra
+# Startup Opts: /etc/default/cassandra
+echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list;
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -;
+sudo apt-get update;
+sudo apt-get install cassandra;
+# If all goes well, the following command should not return an error
+nodetool status;
