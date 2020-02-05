cef84a5c1305a43ee37dc2c2a123ee646ab40095
root <root@server.singlelinktest2.contools-pg0.wisc.cloudlab.us>
2020-02-04 23:27:18 -0600

    Fixed automake on VLAN

--- Tests/VLAN/automake.sh
+++ Tests/VLAN/automake.sh
@@ -1,5 +1,5 @@
 rm -rf ~/snabb/src/VLAN
 rm -rf ~/snabb/src/obj/program/VLAN
 rm ~/snabb/src/snabb
-cp -r ~/VLAN ~/snabb/src/program
+cp -r ~/MultiDimMonitor/Tests/VLAN ~/snabb/src/program
 make -j -C ~/snabb
