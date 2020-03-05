7ba21dca65a0b37352764a7c91bd6876b9683417
Nolan Rudolph <nolanrudolph1@gmail.com>
2020-03-04 20:52:40 -0600

    This should allow transferable project state on git

---
+++ CassandraBuild/debug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ant;
+
+cp ./build/apache-cassandra-3.11.6-SNAPSHOT.jar /usr/share/cassandra/apache-cassandra-3.11.6.jar;
+cp ./build/apache-cassandra-thrift-3.11.6-SNAPSHOT.jar /usr/share/cassandra/apache-cassandra-thrift-3.11.6.jar;
+
+rm -rf /usr/share/cassandra/lib;
+cp -r ./lib /usr/share/cassandra;
+
+service cassandra restart;
+tail -f /var/log/cassandra/system.log | grep "DynamicEndpointSnitch.java";
